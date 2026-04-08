08523ddbd8f8747deca9be20f7daa9b627980d31
jahanvipandey22 <pandeyjahanvi76@gmail.com>
2026-04-08 22:20:34 +0530

    Add files via upload

--- inference.py
+++ inference.py
@@ -8,12 +8,11 @@ env = JobEnv()
 @app.post("/openenv/reset")
 def reset():
     state = env.reset()
-    return {"state": state}
+    return state
 
 @app.post("/openenv/step")
 def step(action: dict):
     act = action.get("action", "learn_skill")
-
     state, reward, done = env.step(act)
 
     return {
